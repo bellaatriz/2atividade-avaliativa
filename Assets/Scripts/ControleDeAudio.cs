70404e16d5d1234aeb7bdb4fa6a4d6661374c6b7
bellaatriz <belatriz4157@gmail.com>
2025-08-12 14:53:50 -0300

    POO 12/08

--- Assets/Scripts/ControleDeAudio.cs
+++ Assets/Scripts/ControleDeAudio.cs
@@ -1,3 +1,4 @@
+using TMPro;
 using UnityEngine;
 using UnityEngine.Audio;
 using UnityEngine.Rendering;
@@ -12,16 +13,31 @@ public class ControleDeAudio : MonoBehaviour
     public Slider volumeSlider;
     public Slider volumeSFXSlider; 
     public Toggle toggleMusica;
+    public TMP_Text textoMusica;
     
     void Start()
     {
         musica = toggleMusica.isOn;
         volume = (int)volumeSlider.value;
+        volumeSFX = (int)volumeSFXSlider.value;
     }
-
-   
+    
     void Update()
     {
+        musica = toggleMusica.isOn;
+        volume = (int)volumeSlider.value;
+        volumeSFX = (int)volumeSFXSlider.value;
+
+        if (musica == true)
+        {
+            textoMusica.text = "Ligado";
+            textoMusica.color = Color.green;
+        }
+        else
+        {
+            textoMusica.text = "Desligado";
+            textoMusica.color = Color.red;
+        }
         
     }
 }
