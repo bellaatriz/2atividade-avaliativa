73147b85fa571159c27d3390531c52b4a7cc6d18
bellaatriz <belatriz4157@gmail.com>
2025-08-12 17:16:04 -0300

    POO 12.08.03

---
+++ Assets/Scripts/Personagem.cs
@@ -0,0 +1,40 @@
+using UnityEngine;
+
+public class Personagem : MonoBehaviour
+{
+    [SerializeField] private int vidas;
+    [SerializeField] private int energia;
+    [SerializeField] private float velocidade;
+
+    public void setVidas(int vidas)
+    {
+        this.vidas = vidas;
+    }
+
+    public int getVidas()
+    {
+        return vidas;
+    }
+
+    public void setEnergia(int energia)
+    {
+        this.energia = energia;
+    }
+
+    public int getEnergia()
+    {
+        return energia;
+    }
+
+    public void setVelocidade(float velocidade)
+    {
+        this.velocidade = velocidade;
+    }
+
+    public float getVelocidade()
+    {
+        return this.velocidade;
+    }
+        
+      
+}
